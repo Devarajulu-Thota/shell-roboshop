bc8ec799037bb4e299bb596d3a4aa192d80e8dd4
Devaraj <devarajthota@gmail.com>
2025-08-20 02:26:24 +0530

    special vars

--- cart.sh
+++ cart.sh
@@ -11,10 +11,10 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR=$PWD
 
-mkdir -p $LOGS_FOLDER 
+mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
-# check the user has root privileges or not 
+# check the user has root priveleges or not
 if [ $USERID -ne 0 ]
 then
     echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
@@ -23,7 +23,7 @@ else
     echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
-# validate function takes input as exit status, what command they tried to install
+# validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
     if [ $1 -eq 0 ]
     then
@@ -50,7 +50,6 @@ then
     VALIDATE $? "Creating roboshop system user"
 else
     echo -e "System user roboshop already created ... $Y SKIPPING $N"
-
 fi
 
 mkdir -p /app 
@@ -62,24 +61,20 @@ VALIDATE $? "Downloading cart"
 rm -rf /app/*
 cd /app 
 unzip /tmp/cart.zip &>>$LOG_FILE
-VALIDATE $? "Unzipping cart"
+VALIDATE $? "unzipping cart"
 
 npm install &>>$LOG_FILE
-VALIDATE $? "Installing dependencies"
+VALIDATE $? "Installing Dependencies"
 
 cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service
 VALIDATE $? "Copying cart service"
 
 systemctl daemon-reload &>>$LOG_FILE
-systemctl enable cart &>>$LOG_FILE
+systemctl enable cart  &>>$LOG_FILE
 systemctl start cart
 VALIDATE $? "Starting cart"
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
-echo -e "Script execution completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
-
-
-
-
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
