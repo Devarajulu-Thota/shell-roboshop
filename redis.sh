80453a175dd404fc36376890c2060a4d86e75e4c
Devaraj <devarajthota@gmail.com>
2025-08-20 01:45:15 +0530

    special vars

--- redis.sh
+++ redis.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+START_TIME=$(date +%s)
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -34,7 +35,7 @@ VALIDATE(){
 }
 
 dnf module disable redis -y &>>$LOG_FILE
-VALIDATE $? "Disabling redis"
+VALIDATE $? "Disabling default redis version"
 
 dnf module enable redis:7 -y &>>$LOG_FILE
 VALIDATE $? "Enabling redis:7"
@@ -42,12 +43,16 @@ VALIDATE $? "Enabling redis:7"
 dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "Installing redis"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
-VALIDATE $? "Editing redis conf file for remote connections"
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Edited redis.conf to accept remote connections"
 
 systemctl enable redis &>>$LOG_FILE
 VALIDATE $? "Enabling redis"
 
-systemctl start redis 
-VALIDATE $? "Starting redis"
+systemctl start redis &>>$LOG_FILE
+VALIDATE $? "Started redis"
 
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "Script execution completed successfully, $Y time taken: $TOTAL_TIME $N" | tee -a $LOG_FILE
